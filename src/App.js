5af6d92c814ef31a5ba5ad1d8a4973303a240a13
Clea Godji <clea.godji@gmail.com>
2019-04-02 02:02:32 +0100

    Fin de react 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
-import logo from "./avion.png";
+import logo from "./homer work.png";
 import "./App.css";
-
 import Quotes from "./Quotes";
+import Lamp from "./Lamp.js"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true,
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
   render() {
+    const currentlogoclassname = this.state.working ? 'App-logo' : 'App-logo-2'
+    const currentlogo = this.state.working ? "https://i.pinimg.com/originals/89/cc/37/89cc370d4ac5c360a4ab1a83156e5d91.png" : "https://seeklogo.com/images/H/homer-logo-2B4611ED99-seeklogo.com.png"
+    const currentworking = this.state.working ? "Homer work" : "Let's Party!"
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Travels</h1>
+          <img src={currentlogo} className={currentlogoclassname} alt="logo" />
+          <h1>{currentworking}</h1>
+          <button
+            onClick={this.handleClick}
+          >What does Homer?
+</button>
         </header>
+        <Lamp on />
+        <Lamp />
         <Quotes />
-      </div>
+      </div >
     );
   }
 
-  
+
 }
 
 export default App;
